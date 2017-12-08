4d7c8f2387906e4a3a5e42c83612ce6d39bf9795
Alexander Alkhimyonok <buckartz@gmail.com>
2017-12-09 02:37:36 +0300

    3 - DanceFloor class

---
+++ src/com/gmail/buckartz/DanceFloor.java
@@ -0,0 +1,14 @@
+package com.gmail.buckartz;
+
+public class DanceFloor {
+
+    private Integer size;
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+}
